6a2304297c015faf1dd3f8c9967d4b5da467fe1a
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-04-07 18:42:57 -0700

    Added documentation and request skeleton

--- index.js
+++ index.js
@@ -9,20 +9,56 @@ app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
 app.get('/', (req,res) => {
-    res.send('Welcome to my app!');
+    res.status(200).send('Welcome to my app!');
 });
 
 app.get('/movies', (req,res) => {
-    res.send('Top 10 movies coming soon!');
+    res.status(200).send('Movie catalogue coming soon!');
 });
 
-app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with super top-secret content.');
+app.get('/movies/favorites', (req,res) => {
+    res.status(200).send('Favorites page coming soon!');
+});
+
+app.get('/movies/:title', (req,res) => {
+    let movie = req.params.title;
+    res.status(200).send(movie + '? Never heard of it, but I\'m sure it\'s a good one!');
+});
+
+app.get('/genres/:name', (req,res) => {
+    let genre = req.params.name;
+    res.status(200).send(genre + '? Never watched any of those.');
+});
+
+app.get('/directors/:name', (req,res) => {
+    let director = req.params.name;
+    res.status(200).send(director + '? Never heard of them.');
+});
+
+app.get('/users/register', (req,res) => {
+    res.status(200).send('Thank you for your interest in my site! Unfortunately it\'s just a skeleton right now!');
+});
+
+app.get('/users/update', (req,res) => {
+    res.status(200).send('My site isn\'t ready yet, so I\'m pretty sure you\'re not a user.');
+});
+
+app.get('/users/deregister', (req,res) => {
+    res.status(200).send('My site isn\'t even ready yet and I\'m already catching flak. :(');
+});
+
+app.post('/movies/favorites/add/:title', (req,res) => {
+    let movie = req.params.title;
+    res.status(201).send(movie + '? I\'m sure it\'s great, but the favorites page isn\'t ready yet.');
+});
+
+app.delete('/movies/favorites/remove/:title', (req,res) => {
+    let movie = req.params.title;
+    res.status(201).send(movie + ' is no longer your favorite? Well I\'m sorry you feel that way, but the favorites page isn\'t ready yet.');
 });
 
 app.listen(8080, () => {
