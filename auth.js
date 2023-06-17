fce6f5420ac10192eefa67eeb49e6e4f5e19a74d
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-06-17 14:46:00 -0700

    Revert "Update auth.js"
    This reverts commit 4ce096312f6c002f7c179dee1692a72af162a238.

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
 const jwtSecret = 'MakingApps';
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const { User } = require('./models');
 
 require('./passport');
 
@@ -18,7 +17,7 @@ module.exports = (router) => {
         passport.authenticate('local', {session: false}, (error, user, info)=> {
             if(error || !user) {
                 return res.status(400).json({
-                    message: user,
+                    message: req,
                     user: user
                 });
             }
